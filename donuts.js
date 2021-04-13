ca2da39769673422529e3655e54f78d8281dfbd3
SashaVM <sashavictoriam@icloud.com>
2021-04-13 14:48:23 -0700

    "create donut" and "random order"

--- donuts.js
+++ donuts.js
@@ -5,7 +5,8 @@ What would you like to do?
 (3) Create new donut type
 (4) Add donuts to inventory
 (5) Order donuts
-(6) Quit program`;
+(6) Generate random orders 
+(7) Quit program`;
 
 class donutData {
 	donutName;
@@ -51,47 +52,20 @@ function mainMenu() {
 			printRevenue(shopRevenue, shopInventory);
 			break;
 		case "3":
-			//createNewDonut();
+			createNewDonut(shopInventory);
 			break;
 		case "4":
-			let donutToAdd = getDonutChoice(shopInventory, false);
-			if(donutToAdd === null){
-				alert("Action cancelled");
-				break;
-			}
-			let quantityToAdd = getDonutQuantity(donutToAdd, false);
-			if(quantityToAdd === 0){
-				alert("Action cancelled")
-				break;
-			}
-			let additionConfirmed = confirm(`${quantityToAdd} ${donutToAdd["donutName"]}(s) will be added to shop inventory`);
-			if(additionConfirmed){
-				addDonuts(donutToAdd, quantityToAdd);
-			}
+			addDonuts(shopInventory);
 			break;
 		case "5":
-			let donutToBuy = getDonutChoice(shopInventory, true);
-			if (donutToBuy === null){
-				alert("Order cancelled");
-				break;
-			}
-			let orderQuantity = getDonutQuantity(donutToBuy, true);
-			if (orderQuantity === 0){
-				alert("Order cancelled");
-				break;
-			}
-			let orderPrice = getOrderPrice(donutToBuy, orderQuantity);
-			let orderConfirmed = confirmOrder(donutToBuy, orderQuantity, orderPrice);
-			if(orderConfirmed) {
-				recordOrder(donutToBuy, orderQuantity, orderPrice);
-				alert("Thank you for your purchase!");
-			}
+			orderDonuts(shopInventory);
 			break;
 		case "6":
+			recordRandomOrders(shopRevenue, shopInventory);
+			break;
+		case "7":
 		case null:
 			break menuLoop;
-		case "7":
-			//recordRandomOrders();
 		default:
 			alert("Please enter a number from 1-6")
 			break;
@@ -116,6 +90,177 @@ function printRevenue(revenueTotal, inventoryObject){
 		messageToPrint.push(`${convertToDollars(inventoryObject[donutEntry]["donutIndividualRevenue"])} from ${inventoryObject[donutEntry]["donutQuantitySold"]} ${inventoryObject[donutEntry]["donutName"]}(s) sold`)
 	});
 	alert(messageToPrint.join("\n"))
+	return;
+}
+
+function createNewDonut(inventoryObject){
+	let cancelAlert = "Donut creation cancelled";
+	let progressDisplay = "";
+	let name = "";
+	let price = "";
+	let initialQuantity = "";
+	let key = "";
+	name = getNewDonutName(inventoryObject);
+	if(name === null){
+		alert(cancelAlert);
+		return;
+	}
+	progressDisplay = `New Donut Name: ${name} \n`;
+	price = getNewDonutPrice(progressDisplay);
+	if(price === null){
+		alert(cancelAlert);
+		return;
+	}
+	progressDisplay += `New Donut Price: ${convertToDollars(price)} \n`;
+	initialQuantity = getNewDonutQuantity(progressDisplay);
+	if(initialQuantity === null){
+		alert(cancelAlert);
+		return;
+	}
+	progressDisplay += `New Donut Quantity: ${initialQuantity} \n`;
+	key = getNewDonutKey(inventoryObject, progressDisplay);
+	if(key === null){
+		alert(cancelAlert);
+		return;
+	}
+	progressDisplay += `New Donut Key: ${key} \n`;
+	if( confirm(progressDisplay + "Confirm creation of new donut type?") ){
+		inventoryObject[key] = new donutData(name, price, initialQuantity);
+		alert(`${name} successfully added to shop!`);
+		return;
+	}
+	alert(cancelAlert);
+	return;
+}
+
+function getNewDonutName(inventoryObject){
+	let newDonutName = ""
+	let keyArray = Object.keys(inventoryObject);
+	let inUse = false;
+	while(true){
+		newDonutName = prompt("What is the display name for the new donut?", "");
+		keyArray.forEach(function(donutEntry){
+			if(inventoryObject[donutEntry]["donutName"] === newDonutName){
+				inUse = true;
+			}
+			return;
+		})
+		if(inUse){
+			alert("That name is already in use. Please choose another");
+			continue;
+		}
+		break;
+	}
+	return newDonutName;
+}
+
+function getNewDonutPrice(donutProgress){
+	let price = "";
+	while(true){
+		price = prompt(donutProgress + "What is the price of the new donut?", "");
+		if(price === null){
+			break;
+		}
+		price = Number(price);
+		if( isNaN(price) || price < 0 ){
+			alert("Price must be a positive number");
+			continue;
+		}
+		break;
+	}
+	return price;
+}
+
+function getNewDonutQuantity(donutProgress){
+	let quantity = ""
+	while(true){
+		quantity = prompt(donutProgress + "How many of the new type of donut would you like to add to store inventory?", "");
+		if(quantity === null){
+			break;
+		}
+		quantity = Number(quantity);
+		if( isNaN(quantity) || quantity < 0 || (Math.floor(quantity) != quantity) ){
+			alert("Quantity must be a positive, whole number (Type '0' if you do not want to add donuts)");
+			continue;
+		}
+		break;
+	}
+	return quantity;
+}
+
+function getNewDonutKey(inventoryObject, donutProgress){
+	let newKey = ""
+	while(true){
+		newKey = prompt(donutProgress + "Enter a short, one-word key to easily access the new donut type. \n Key is not case sensitive and cannot already be associated with another donut type", "");
+		if(newKey === null){
+			break;
+		}
+		newKey = newKey.toLowerCase();
+		if (newKey in inventoryObject){
+			let keysInUseMessage = `Sorry, that key is already in use. Please choose a different key. \n All current keys in use are: ${Object.keys(inventoryObject).join(", ")}`
+			alert(keysInUseMessage);
+			continue;
+		}
+		break;
+	}
+	return newKey;
+}
+
+function addDonuts(inventoryObject){
+	let donutToAdd = getDonutChoice(inventoryObject, false);
+	if(donutToAdd === null){
+		alert("Action cancelled");
+		return;
+	}
+	let quantityToAdd = getDonutQuantity(donutToAdd, false);
+	if(quantityToAdd === 0){
+		alert("Action cancelled")
+		return;
+	}
+	let additionConfirmed = confirm(`${quantityToAdd} ${donutToAdd["donutName"]}(s) will be added to shop inventory`);
+	if(additionConfirmed){
+		recordDonutAddition(donutToAdd, quantityToAdd);
+	}
+	return;
+}
+
+function orderDonuts(inventoryObject){
+	let donutToBuy = getDonutChoice(inventoryObject, true);
+	if (donutToBuy === null){
+		alert("Order cancelled");
+		return;
+	}
+	let orderQuantity = getDonutQuantity(donutToBuy, true);
+	if (orderQuantity === 0){
+		alert("Order cancelled");
+		return;
+	}
+	let orderPrice = getOrderPrice(donutToBuy, orderQuantity);
+	let orderConfirmed = confirmOrder(donutToBuy, orderQuantity, orderPrice);
+	if(orderConfirmed) {
+		recordOrder(donutToBuy, orderQuantity, orderPrice);
+		alert("Thank you for your purchase!");
+	}
+	else{
+		alert("Order cancelled")
+	}
+	return;
+}
+
+function recordRandomOrders(revenueTotal, inventoryObject){
+	let keyArray = Object.keys(inventoryObject);
+	let ordersToGenerate = Number( prompt("How many random orders would you like to generate?", "") );
+	if(isNaN(ordersToGenerate)){
+		alert("Not a number; action canclled");
+		return;
+	}
+	for(let i = 0; i < ordersToGenerate; i++){
+		let donutChoice = inventoryObject[keyArray[getRandomNumber(0, (keyArray.length - 1))]];
+		let donutQuantity = getRandomNumber(1, (donutChoice["donutQuantityInStore"]));
+		let orderPrice = getOrderPrice(donutChoice, donutQuantity);
+		recordOrder(donutChoice, donutQuantity, orderPrice);
+	}
+	alert(`${ordersToGenerate} order(s) recorded`)
 	return;
 }
 
@@ -161,7 +306,7 @@ function getDonutQuantity(donutObject, purchase){
 	return quantity;
 }
 
-function addDonuts(donutObject, quantity){
+function recordDonutAddition(donutObject, quantity){
 	donutObject["donutQuantityInStore"] += quantity;
 }
 
@@ -186,3 +331,32 @@ function recordOrder(donutObject, quantity, price){
 function convertToDollars(x){
 	return "$" + Number.parseFloat(x).toFixed(2);
 }
+
+function getRandomNumber(min, max){
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+
+// early version of random orders, decided I didn't have time to include adding more donuts
+// function recordRandomOrders(revenueTotal, inventoryObject){
+// 	let keyArray = Object.keys(inventoryObject);
+// 	let totalDonuts = "";
+// 	let quantityMessage = "";
+// 	let addMoreDonuts = "";
+// 	keyArray.forEach( function(donutEntry){
+// 		totalDonuts += inventoryObject[donutEntry]["donutQuantityInStore"];
+// 	})
+// 	quantityMessage = `There are currently ${totalDonuts} in the store. \n (1) Generate random orders now \n (2) Add more donuts first \n (3) Cancel`;
+// 	while(true){
+// 		let input = prompt(quantityMessage, "")
+// 		switch (input){
+// 			case "1":
+// 				break;
+// 			case "2":
+// 				while( isNaN(addMoreDonuts) || (Math.floor(addMoreDonuts) != addMoreDonuts) ){
+// 					addMoreDonuts = Number(prompt("Type a quantity of donuts to add to all types", ""))
+// 				}
+// 		}
+// 	}
+// }
